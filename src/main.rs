eabd21e4e7cdb6383656ce98ecf072d7ae1fdb5e
Ivan Bogdanov <c3t3o7@gmail.com>
2020-06-27 15:48:20 +0400

    Use board.psq.mg value

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
 use pleco;
+use rand;
+use rand::seq::SliceRandom;
 use std::io::Write;
 
 fn board_pretty_print(board: &pleco::Board) {
@@ -35,18 +37,7 @@ fn board_pretty_print(board: &pleco::Board) {
             let square = pleco::SQ::make(int_to_file(col), int_to_rank(row));
             match board.piece_at_sq(square) {
                 pleco::Piece::None => print!("."),
-                pleco::Piece::BlackKing => print!("♚"),
-                pleco::Piece::BlackQueen => print!("♛"),
-                pleco::Piece::BlackRook => print!("♜"),
-                pleco::Piece::BlackBishop => print!("♝"),
-                pleco::Piece::BlackKnight => print!("♞"),
-                pleco::Piece::BlackPawn => print!("♟︎"),
-                pleco::Piece::WhiteKing => print!("♔"),
-                pleco::Piece::WhiteQueen => print!("♕"),
-                pleco::Piece::WhiteRook => print!("♖"),
-                pleco::Piece::WhiteBishop => print!("♗"),
-                pleco::Piece::WhiteKnight => print!("♘"),
-                pleco::Piece::WhitePawn => print!("♙"),
+                p => print!("{}", p),
             }
             print!(" ");
         }
@@ -66,99 +57,162 @@ fn input(promt: &str) -> String {
     return ret.trim().to_string();
 }
 
+fn shuffled_move_list(it: pleco::MoveList) -> Vec<pleco::BitMove> {
+    let mut rng = rand::thread_rng();
+    let mut y: Vec<pleco::BitMove> = it.iter().map(|x| *x).collect();
+    y.shuffle(&mut rng);
+    return y;
+}
+
 fn alpha_beta(
     board: &pleco::Board,
     depth: i32,
     mut alpha: f64,
     mut beta: f64,
     maximize: bool,
-) -> f64 {
-    //println!("experiment_board:");
-    //board_pretty_print(&board);
-    if depth <= 0 {
-        return board_value(&board);
-    }
-    if board.checkmate() {
-        if maximize {
-            return 100.;
-        } else {
-            return -100.;
+    verbose: bool,
+) -> (Option<pleco::BitMove>, f64) {
+    if depth <= 0 || board.checkmate() {
+        if verbose {
+            println!("experiment_board:");
+            board_pretty_print(&board);
+            println!("experiment value = {}", board_value(&board));
         }
-    }
-    if board.stalemate() {
-        return 0.;
+        return (None, board_value(&board));
     }
     let mut experiment_board = board.clone();
+    let mut best_move: Option<pleco::BitMove> = None;
     if maximize {
         let mut best_value = f64::NEG_INFINITY;
-        for m in board.generate_moves() {
+        for m in shuffled_move_list(board.generate_moves()) {
             experiment_board.apply_move(m);
-            let value = alpha_beta(&experiment_board, depth - 1, alpha, beta, !maximize);
+            let (_, value) = alpha_beta(
+                &experiment_board,
+                depth - 1,
+                alpha,
+                beta,
+                !maximize,
+                verbose,
+            );
+            if verbose {
+                println!(
+                "Max: depth = {}, experiment move = {}, value = {}, best_move = {}, best_value = {}",
+                depth,
+                m,
+                value,
+                if let Some(b) = best_move {b.to_string()}else {"None".to_owned()},
+                best_value
+            );
+            }
             experiment_board.undo_move();
-            best_value = best_value.max(value);
+            if value > best_value {
+                best_value = value;
+                best_move = Some(m);
+            }
             alpha = alpha.max(best_value);
             if alpha >= beta {
                 break;
             }
         }
-        return best_value;
+        return (best_move, best_value);
     } else {
         let mut best_value = f64::INFINITY;
-        for m in board.generate_moves() {
+        for m in shuffled_move_list(board.generate_moves()) {
             experiment_board.apply_move(m);
-            let value = alpha_beta(&experiment_board, depth - 1, alpha, beta, !maximize);
+            let (_, value) = alpha_beta(
+                &experiment_board,
+                depth - 1,
+                alpha,
+                beta,
+                !maximize,
+                verbose,
+            );
+            if verbose {
+                println!(
+                "Min: depth = {}, experiment move = {}, value = {}, best_move = {}, best_value = {}",
+                depth,
+                m,
+                value,
+                if let Some(b) = best_move {b.to_string()}else {"None".to_owned()},
+                best_value
+            );
+            }
             experiment_board.undo_move();
-            best_value = best_value.min(value);
+            if value < best_value {
+                best_value = value;
+                best_move = Some(m);
+            }
             beta = beta.min(best_value);
             if alpha >= beta {
                 break;
             }
         }
-        return best_value;
+        return (best_move, best_value);
     }
 }
 
 // The more value - the more white wins.
-fn board_value(board: &pleco::Board) -> f64 {
-    let fen_str2 = board.fen();
-    let fen_str = fen_str2.split_whitespace().next().unwrap();
-    let pawn_diff = fen_str.matches("P").count() as f64 - fen_str.matches("p").count() as f64;
-    let rook_diff = fen_str.matches("R").count() as f64 - fen_str.matches("r").count() as f64;
-    let knight_diff = fen_str.matches("N").count() as f64 - fen_str.matches("n").count() as f64;
-    let bishop_diff = fen_str.matches("B").count() as f64 - fen_str.matches("b").count() as f64;
-    let queen_diff = fen_str.matches("Q").count() as f64 - fen_str.matches("q").count() as f64;
-    return 1. * pawn_diff + 3. * bishop_diff + 3. * knight_diff + 5. * rook_diff + 9. * queen_diff;
-}
-
-fn get_best_move(board: &pleco::Board, depth: i32) -> pleco::BitMove {
-    let mut min_value = f64::INFINITY;
-    let mut experiment_board = board.clone();
-    let mut min_move = pleco::BitMove::null();
-    for m in board.generate_moves() {
-        experiment_board.apply_move(m);
-        let value = alpha_beta(
-            &experiment_board,
-            depth,
-            f64::NEG_INFINITY,
-            f64::INFINITY,
-            false,
-        );
-        println!("experiment move = {}, value = {}", m, value);
-        experiment_board.undo_move();
-        if value < min_value {
-            min_value = value;
-            println!("min_value = {}", min_value);
-            min_move = m;
+fn board_value2(board: &pleco::Board) -> f64 {
+    if board.checkmate() {
+        if board.turn() == pleco::Player::Black {
+            return 100.;
+        } else {
+            return -100.;
         }
     }
-    return min_move;
+    let mut total_value = 0.;
+    let fen_str2 = board.fen();
+    let fen_str = fen_str2.split_whitespace().next().unwrap();
+    total_value += 1.
+        * (fen_str.matches("P").count() as f64
+            - fen_str.matches("p").count() as f64);
+    total_value += 5.
+        * (fen_str.matches("R").count() as f64
+            - fen_str.matches("r").count() as f64);
+    total_value += 3.
+        * (fen_str.matches("N").count() as f64
+            - fen_str.matches("n").count() as f64);
+    total_value += 3.
+        * (fen_str.matches("B").count() as f64
+            - fen_str.matches("b").count() as f64);
+    total_value += 9.
+        * (fen_str.matches("Q").count() as f64
+            - fen_str.matches("q").count() as f64);
+    if board.in_check() {
+        if board.turn() == pleco::Player::Black {
+            total_value += 50.;
+        } else {
+            total_value += -50.;
+        }
+    }
+    return total_value;
+}
+
+fn board_value(board: &pleco::Board) -> f64 {
+    return board.psq().mg() as f64;
+}
+
+fn get_best_move(board: &pleco::Board, depth: i32, verbose: bool) -> pleco::BitMove {
+    // Computer plays for black. It need to find move with minimal value.
+    let (best_move, value) = alpha_beta(
+        &board,
+        depth,
+        f64::NEG_INFINITY,
+        f64::INFINITY,
+        false,
+        verbose,
+    );
+    println!("computer value = {}", value);
+    return best_move.unwrap();
 }
 
 fn main() {
+    const DEPTH: i32 = 6;
+    const VERBOSE: bool = false;
+
     let mut game_board = pleco::Board::default();
-    let mut move_counter = 1;
     loop {
-        println!("===== {}-th move:", move_counter);
+        println!("===== {}-th move:", game_board.ply());
         board_pretty_print(&game_board);
         let mut legal_moves: Vec<String> = game_board
             .generate_moves()
@@ -166,7 +220,11 @@ fn main() {
             .map(|x| x.to_string())
             .collect();
         legal_moves.sort();
-        println!("legal_moves = {:?}", legal_moves);
+        println!(
+            "legal_moves = {:?}, len = {}",
+            legal_moves,
+            legal_moves.len()
+        );
         loop {
             let user_move = input("Type your move: ");
             let b = game_board.apply_uci_move(&user_move);
@@ -185,10 +243,9 @@ fn main() {
             break;
         }
 
-        let computer_move = get_best_move(&game_board, 6);
+        let computer_move = get_best_move(&game_board, DEPTH, VERBOSE);
         println!("computer move = {}", computer_move);
         game_board.apply_move(computer_move);
-
         if game_board.checkmate() {
             println!("Chechmate! Computer won!");
             break;
@@ -196,6 +253,5 @@ fn main() {
             println!("Stalemate! Game over.");
             break;
         }
-        move_counter += 1;
     }
 }

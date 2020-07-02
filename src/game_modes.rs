19b1521eec4a602f77e21110f13c2e1f2e1a4c51
Ivan Bogdanov <c3t3o7@gmail.com>
2020-07-02 19:09:20 +0400

    Add option to evaluate users move

--- src/game_modes.rs
+++ src/game_modes.rs
@@ -1,5 +1,6 @@
 use crate::alpha_beta::get_best_move;
 use crate::board_pretty_print::board_pretty_print;
+use crate::game_settings::GameSettings;
 use pleco;
 use std::io::Write;
 
@@ -13,12 +14,12 @@ fn input(promt: &str) -> String {
     return ret.trim().to_string();
 }
 
-pub fn computer_with_computer(depth: i32) {
+pub fn computer_with_computer(settings: &GameSettings) {
     let mut game_board = pleco::Board::default();
     loop {
         board_pretty_print(&game_board);
 
-        let white_best = get_best_move(&game_board, depth, true);
+        let white_best = get_best_move(&game_board, settings.depth, true);
         println!("White move = {}, value = {}", white_best.0, white_best.1);
         game_board.apply_move(white_best.0);
         if game_board.checkmate() {
@@ -29,7 +30,7 @@ pub fn computer_with_computer(depth: i32) {
             break;
         }
 
-        let black_best = get_best_move(&game_board, depth, false);
+        let black_best = get_best_move(&game_board, settings.depth, false);
         println!("black move = {}, value = {}", black_best.0, black_best.1);
         game_board.apply_move(black_best.0);
         if game_board.checkmate() {
@@ -42,7 +43,7 @@ pub fn computer_with_computer(depth: i32) {
     }
 }
 
-pub fn white_user_with_black_computer(depth: i32) {
+pub fn white_user_with_black_computer(settings: &GameSettings) {
     let mut game_board = pleco::Board::default();
     loop {
         board_pretty_print(&game_board);
@@ -57,11 +58,13 @@ pub fn white_user_with_black_computer(depth: i32) {
             legal_moves,
             legal_moves.len()
         );
-        let white_best = get_best_move(&game_board, depth, true);
-        println!(
-            "White best move = {}, value = {}",
-            white_best.0, white_best.1
-        );
+        if settings.evaluate_user {
+            let white_best = get_best_move(&game_board, settings.depth, true);
+            println!(
+                "White best move = {}, value = {}",
+                white_best.0, white_best.1
+            );
+        }
         loop {
             let user_move = input("Type white move: ");
             if user_move == "u" {
@@ -90,7 +93,7 @@ pub fn white_user_with_black_computer(depth: i32) {
             break;
         }
 
-        let black_best = get_best_move(&game_board, depth, false);
+        let black_best = get_best_move(&game_board, settings.depth, false);
         println!("black move = {}, value = {}", black_best.0, black_best.1);
         game_board.apply_move(black_best.0);
         if game_board.checkmate() {
@@ -103,10 +106,10 @@ pub fn white_user_with_black_computer(depth: i32) {
     }
 }
 
-pub fn black_user_with_white_computer(depth: i32) {
+pub fn black_user_with_white_computer(settings: &GameSettings) {
     let mut game_board = pleco::Board::default();
     loop {
-        let white_best = get_best_move(&game_board, depth, true);
+        let white_best = get_best_move(&game_board, settings.depth, true);
         println!("white move = {}, value = {}", white_best.0, white_best.1);
         game_board.apply_move(white_best.0);
         board_pretty_print(&game_board);
@@ -129,11 +132,13 @@ pub fn black_user_with_white_computer(depth: i32) {
             legal_moves,
             legal_moves.len()
         );
-        let black_best = get_best_move(&game_board, depth, false);
-        println!(
-            "Black best move = {}, value = {}",
-            black_best.0, black_best.1
-        );
+        if settings.evaluate_user {
+            let black_best = get_best_move(&game_board, settings.depth, false);
+            println!(
+                "Black best move = {}, value = {}",
+                black_best.0, black_best.1
+            );
+        }
         loop {
             let user_move = input("Type black move: ");
             if user_move == "u" {
@@ -163,4 +168,3 @@ pub fn black_user_with_white_computer(depth: i32) {
         }
     }
 }
-
---
+++ src/game_settings.rs
@@ -0,0 +1,6 @@
+
+
+pub struct GameSettings {
+    pub depth: i32,
+    pub evaluate_user: bool,
+}

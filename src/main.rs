8826c1cfa812046c8b6b079eced2affdda0c3bc7
vehlwn <36479428+vehlwn@users.noreply.github.com>
2022-06-22 17:02:21 +0400

    Add option to evaluate users move

--- src/main.rs
+++ src/main.rs
@@ -2,12 +2,14 @@ mod alpha_beta;
 mod board_pretty_print;
 mod board_value;
 mod game_modes;
+mod game_settings;
 mod shuffled_move_list;
 
 use clap;
 use game_modes::black_user_with_white_computer;
 use game_modes::computer_with_computer;
 use game_modes::white_user_with_black_computer;
+use game_settings::GameSettings;
 
 fn main() {
     let matches = clap::App::new("chess-alpha-beta")
@@ -34,15 +36,34 @@ fn main() {
                 .possible_values(&["cc", "wubc", "buwc"])
                 .default_value("wubc"),
         )
+        .arg(
+            clap::Arg::with_name("evaluate_user")
+                .long("evaluate-user")
+                .short("e")
+                .help("show user's potentially best move when playing with computer")
+                .possible_values(&["0", "1"])
+                .default_value("0"),
+        )
        .get_matches();
     let depth: i32 = matches.value_of("depth").unwrap().parse().unwrap();
+    let evaluate_user: bool = matches
+        .value_of("evaluate_user")
+        .unwrap()
+        .parse::<i32>()
+        .unwrap()
+        != 0;
     println!("depth = {}", depth);
 
-   let mode = matches.value_of("mode").unwrap();
+    let settings = GameSettings {
+        depth,
+        evaluate_user,
+    };
+
+    let mode = matches.value_of("mode").unwrap();
     match mode {
-        "cc" => computer_with_computer(depth),
-        "wubc" => white_user_with_black_computer(depth),
-        "buwc" => black_user_with_white_computer(depth),
+        "cc" => computer_with_computer(&settings),
+        "wubc" => white_user_with_black_computer(&settings),
+        "buwc" => black_user_with_white_computer(&settings),
         x => panic!("Unknown mode: '{}'", x),
     };
 }

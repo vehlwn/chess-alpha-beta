06d9d1762bed6577e3b6b9872147fb23a835ea89
vehlwn <36479428+vehlwn@users.noreply.github.com>
2022-06-22 17:02:22 +0400

    Create type alias for best value

--- src/board_value.rs
+++ src/board_value.rs
@@ -1,23 +1,28 @@
 use crate::alpha_beta::AlphaBetaParameters;
+use crate::alpha_beta::ValueType;
 use pleco;
 
 // Computer plays for black. It need to find move with minimal value.
 // The more value - the more white wins.
-pub fn board_value(board: &pleco::Board, params: &AlphaBetaParameters) -> f64 {
+pub fn board_value(board: &pleco::Board, params: &AlphaBetaParameters) -> ValueType {
     return board_value_impl(board, params);
 }
 
-fn board_value_impl(board: &pleco::Board, params: &AlphaBetaParameters) -> f64 {
-    let mut mate_score = 0.;
+fn board_value_impl(
+    board: &pleco::Board,
+    params: &AlphaBetaParameters,
+) -> ValueType {
+    let mut mate_score = ValueType::default();
     if board.checkmate() {
         // The less moves to mate - the more mate bonus.
         // current_depth at the start of an algorithm is equal to max_depth
         // and decreases to the leaf nodes of the solution tree.
-        let moves_to_mate_bonus = params.current_depth as f64 * 1.0e5;
+        let moves_to_mate_bonus =
+            params.current_depth as ValueType * 1.0e5 as ValueType;
         if board.turn() == pleco::Player::Black {
-            mate_score = 1.0e6 + moves_to_mate_bonus;
+            mate_score = 1.0e6 as ValueType + moves_to_mate_bonus;
         } else {
-            mate_score = -1.0e6 - moves_to_mate_bonus;
+            mate_score = -1.0e6 as ValueType - moves_to_mate_bonus;
         }
     }
     return if std::cmp::min(
@@ -28,6 +33,6 @@ fn board_value_impl(board: &pleco::Board, params: &AlphaBetaParameters) -> f64 {
         board.psq().mg()
     } else {
         board.psq().eg()
-    } as f64
+    } as ValueType
         + mate_score;
 }
